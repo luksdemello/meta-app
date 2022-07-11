a6f14e125d5d1cbda5c0041f8c0d2a03f0ff571c
Lucas de mello <lucas_mello6@hotmail.com>
2022-07-11 14:26:19 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
-  
+
   return (
-   <div>
-    <h1>Olá mundo</h1>
-   <NotificationButton />
-   </div>
+    <>
+      <Header />
+     <main>
+      <section id="sales">
+        <div className="appmeta-container">
+          <SalesCard />
+        </div>
+      </section>
+     </main>
+    </>
   )
 }
 
