aa64cb5f27d408d698fc6ff66b2b28080c269cc3
Lucas de mello <lucas_mello6@hotmail.com>
2022-07-11 14:11:18 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification-icon.svg';
+
+import './styles.css'
+
+function NotificationButton() {
+  return (
+    <div className='appmeta-red-btn'>
+      <img src={icon}  />
+    </div>
+  )
+}
+
+export default NotificationButton
